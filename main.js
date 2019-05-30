bb1f03f2e7389b632c22ae1efd3ee02399e4f9f1
Kacper <kacerkacer0@gmail.com>
2019-05-30 16:47:10 +0200

    fix

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 const btn = document.querySelector("#btn");
 btn.addEventListener("click", function() {
-  let wynik = waga.value / Math.pow(wzrost.value, 2);
-  document.querySelector("#wynik").innerHTML = wynik.toFixed(2);
-  document.getElementById("wynik").style.display = "block";
-  if (wynik >= 30) {
-    document.getElementById("wynik").style.backgroundColor = "#D12124";
-  } else if (wynik >= 25) {
-    document.getElementById("wynik").style.backgroundColor = "#EF930A";
-  } else if (wynik >= 18.5) {
-    document.getElementById("wynik").style.backgroundColor = "#4DA93E";
+  let result = weight.value / Math.pow(height.value, 2);
+  let outcome = document.querySelector("#result");
+
+  outcome.innerHTML = result.toFixed(2);
+  outcome.style.display = "block";
+  if (result >= 30) {
+    outcome.style.backgroundColor = "#D12124";
+  } else if (result >= 25) {
+    outcome.style.backgroundColor = "#EF930A";
+  } else if (result >= 18.5) {
+    outcome.style.backgroundColor = "#4DA93E";
   } else {
-    document.getElementById("wynik").style.backgroundColor = "#36A5DD";
+    outcome.style.backgroundColor = "#36A5DD";
   }
 });
